7f4bd8c656a5f8ce28a791982e6bdf9009defbec
kylim99 <113866973+kylim99@users.noreply.github.com>
2025-02-18 16:27:34 +0900

    Revert "Feature/product quantity"

--- src/main/java/com/teamsparta14/order_service/config/PropertyConfig.java
+++ src/main/java/com/teamsparta14/order_service/config/PropertyConfig.java
@@ -1,4 +1,4 @@
-package com.teamsparta14.order_service.config;
+package com.teamsparta14.order_service.global;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
