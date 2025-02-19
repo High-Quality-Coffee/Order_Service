c89c461cc13e63d3e841c1c8ba5413e10411bcd8
kylim99 <zxzx05050@naver.com>
2025-02-19 14:48:44 +0900

    [docs] swagger API docs 기능 추가

--- src/main/java/com/teamsparta14/order_service/product/repository/DescriptionRepository.java
+++ src/main/java/com/teamsparta14/order_service/product/repository/DescriptionRepository.java
@@ -2,8 +2,10 @@ package com.teamsparta14.order_service.product.repository;
 
 import com.teamsparta14.order_service.product.entity.Description;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface DescriptionRepository extends JpaRepository<Description, UUID> {
 }
